66536530220e036f61a3cefa58db3ee5b5f8c523
power <1410837981@qq.com>
2025-07-10 21:19:42 +0800

    feat： 标签页新增

---
+++ src/api/admin/tag.ts
@@ -0,0 +1,35 @@
+import { request } from '@/utils/request'
+
+interface TagListType {
+  current: number
+  endDate: string
+  name: string
+  size: number
+  startDate: string
+}
+// 获取标签分页数据
+export function getTagPageList(data: TagListType) {
+  return request<[]>({
+    url: '/admin/tag/list',
+    method: 'post',
+    data,
+  })
+}
+
+export function addTag(data: { tags: string[] }) {
+  return request<[]>({
+    url: '/admin/tag/add',
+    method: 'post',
+    data,
+  })
+}
+
+export function deleteTag(id: number) {
+  return request<[]>({
+    url: '/admin/tag/delete',
+    method: 'post',
+    data: {
+      id,
+    },
+  })
+}
